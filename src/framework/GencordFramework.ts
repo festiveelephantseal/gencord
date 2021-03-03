8d0ff1695d0f535e8b5aa87669c7cf28a2179f8b
Milo123459 <milo5huntington@gmail.com>
2021-03-03 21:21:03 +0000

    lint

--- src/framework/GencordFramework.ts
+++ src/framework/GencordFramework.ts
@@ -1,23 +1,23 @@
-import { Client, ClientOptions } from '../';
+import { Client, ClientOptions } from "../";
 
 export interface Directories {
-    commands: string;
-    events: string;
-};
+  commands: string;
+  events: string;
+}
 
 export interface Options extends ClientOptions {
-    directories: Directories;
+  directories: Directories;
 }
 export class Framework extends Client {
-    public directories: Directories;
-    public options: Options;
+  public directories: Directories;
+  public options: Options;
 
-    public constructor(options: Options) {
-        super({
-            token: options.token,
-            intents: options.intents || 513
-        });
-        this.options = options;
-        this.directories = options.directories;
-    };
+  public constructor(options: Options) {
+    super({
+      token: options.token,
+      intents: options.intents || 513,
+    });
+    this.options = options;
+    this.directories = options.directories;
+  }
 }
