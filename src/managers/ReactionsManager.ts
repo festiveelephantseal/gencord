75ae4a7ba0d2ff3cf9cbce50c8a8e9d2d0df2d36
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-05 17:44:50 -0800

    feat(ReactionManager): start work on create method

--- src/managers/ReactionsManager.ts
+++ src/managers/ReactionsManager.ts
@@ -1,13 +1,23 @@
 import { Client } from "../client/Client";
+import { ChannelManager } from "./ChannelManager";
+import { Message } from "../structures/Message";
 
 export class ReactionsManager {
   private client: Client;
+  private channel: ChannelManager;
+  private message: Message;
 
   public constructor(client: Client) {
     this.client = client;
   }
 
-  public async create(): Promise<void> {}
+  public async create(emoji: string): Promise<void> {
+    return await this.client.handler.fetch({
+      method: "PUT",
+      endpoint: `/channels/${this.channel.id}/messages/${this.message.id}/reactions/${emoji}/@me`,
+    });
+  }
+
   public async get(): Promise<void> {}
   public async deleteOwn(): Promise<void> {}
   public async deleteUser(): Promise<void> {}
