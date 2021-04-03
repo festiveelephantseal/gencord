044c752e181647566ae2b7afb1b404ace9cb64a4
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-04-02 19:17:53 -0700

    feat(VoiceState): add voicestate structure

--- src/structures/VoiceState.ts
+++ src/structures/VoiceState.ts
@@ -1,3 +1,15 @@
 import { GuildMember } from "./GuildMember";
 
-export class VoiceState extends GuildMember {}
+export class VoiceState extends GuildMember {
+  public guild_id: string;
+  public channel_id: string;
+  public user_id: string;
+  public member: GuildMember;
+  public session_id: string;
+  public deaf: boolean;
+  public mute: boolean;
+  public self_deaf: boolean;
+  public self_mute: boolean;
+  public self_stream: boolean;
+  public suppress: boolean;
+}
