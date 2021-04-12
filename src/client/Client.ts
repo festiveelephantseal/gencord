69db3afe014bf0aabbb99e9b02d22b7328df6b27
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-04-11 21:27:43 -0700

    feat(WebSocketManager): finish up ws manager

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,3 +1,41 @@
 import { WebSocketManager } from "../ws/WebSocketManager";
+import { ClientOptions } from "../typings/ClientOptions";
 
-export class Client extends WebSocketManager {}
+export class Client extends WebSocketManager {
+  public options: ClientOptions;
+  public token: string;
+
+  public constructor(options: ClientOptions) {
+    super();
+
+    this.options = options;
+    this.token = options.token;
+    this.connect();
+  }
+
+  public identify() {
+    this.socket.send(
+      JSON.stringify({
+        op: 2,
+        d: {
+          token: this.options.token,
+          intents: this.options.intents,
+          properties: {
+            $os: "linux",
+            $browser: "gencord",
+            $device: "gencord",
+          },
+          presence: {
+            status: this.options.status,
+            activities: [
+              {
+                name: this.options.activityName,
+                type: this.options.activityType,
+              },
+            ],
+          },
+        },
+      })
+    );
+  }
+}
