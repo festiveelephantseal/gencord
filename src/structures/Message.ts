ca2c6cf706502b8e54f28d51c28fa7aea7955cc9
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-04-12 12:05:32 -0700

    feat(Message): channel type on message

--- src/structures/Message.ts
+++ src/structures/Message.ts
@@ -1,17 +1,21 @@
 import { Client } from "../client/Client";
 import { Base } from "./Base";
+import { TextChannel } from "./TextChannel";
 import {
   MessageDeleteOptions,
   MessagePinOptions,
 } from "../typings/MessageOptions";
+import { DMChannel } from "./DMChannel";
+import { NewsChannel } from "./NewsChannel";
+import { GuildMember } from "./GuildMember";
 
 export class Message extends Base {
   public constructor(
     private id: string,
     //private type
-    //private channel
+    private channel: TextChannel | DMChannel | NewsChannel,
     private tts: boolean,
-    //private member,
+    private member: GuildMember,
     private content: string,
     private timestamp: Date,
     private editedAt: Date,
@@ -23,20 +27,41 @@ export class Message extends Base {
   public get _client(): Client {
     return this.client;
   }
+
   public get _id(): string {
     return this.id;
   }
+
   public get _timestamp(): Date {
     return this.timestamp;
   }
+
   public get _nonce(): string | number {
     return this.nonce;
   }
+
   public get _content(): string {
     return this.content;
   }
+
   public get _tts(): boolean {
     return this.tts;
+  }
+
+  public get _editedAt(): Date {
+    return this.editedAt;
+  }
+
+  public get _mentionedEveryone(): boolean {
+    return this.mentionedEveryone;
+  }
+
+  public get _member(): GuildMember {
+    return this.member;
+  }
+
+  public get _channel(): TextChannel | DMChannel | NewsChannel {
+    return this.channel;
   }
 
   public async delete(options: MessageDeleteOptions): Promise<Message> {
